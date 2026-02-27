886cb0b2c0a4d6ebb922d8ede8e4da58a07b0e18
zhlmi25 <rahmanzulhilmi@gmail.com>
2026-02-27 14:47:07 +0800

    Update launch and URDF files to reflect project name change and enhance material definitions

--- launch/navigation.launch.py
+++ launch/navigation.launch.py
@@ -28,7 +28,7 @@ from nav2_common.launch import RewrittenYaml
 
 def generate_launch_description():
     # Get the launch directory
-    bringup_dir = get_package_share_directory('fielder_description')
+    bringup_dir = get_package_share_directory('fielder_gz')
 
     namespace = LaunchConfiguration('namespace')
     use_sim_time = LaunchConfiguration('use_sim_time')
